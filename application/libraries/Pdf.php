e6d4d5af08492b4abd0b16b26e7ddc9c1d400e19
sreenavc <sreenavc@datatemplate.com>
2022-12-08 10:23:30 +0530

    Pushing tcpdf library file

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -4,10 +4,14 @@
 * start Pdf.php file
 * Location: ./application/libraries/Pdf.php
 */
- if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once dirname(__FILE__) . '\TCPDF-main\examples\tcpdf_include.php';
-require_once dirname(__FILE__) . '\TCPDF-main\tcpdf.php';
+ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ //error_reporting(E_ALL);
+
+//require_once dirname(__FILE__) . '\TCPDF-main\examples\tcpdf_include.php' or die("exiting"); 
+require_once dirname(__FILE__) . '/TCPDF-main/examples/tcpdf_include.php' ; 
+
+require_once dirname(__FILE__) . '/TCPDF-main/tcpdf.php';
  
 
 class Pdf extends TCPDF
